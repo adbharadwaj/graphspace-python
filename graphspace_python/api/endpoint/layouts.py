b63dfbd01aa430230fae4ba95ba2ad7a09bdf960
Sandeep <sandeep.mahapatra.5@gmail.com>
2017-06-22 19:46:43 +0530

    updated layout methods, layout obj class inherits GSLayout class

--- graphspace_python/api/endpoint/layouts.py
+++ graphspace_python/api/endpoint/layouts.py
@@ -6,189 +6,48 @@ class Layouts(object):
 	def __init__(self, client):
 		self.client = client
 
-	def post_graph_layout(self, graph_id, layout_name, positions_json=None, style_json=None, is_shared=None):
+	def post_graph_layout(self, graph_id, layout, is_shared=None):
 		"""Create a layout for the graph with given graph_id.
 
-		:param style_json: JSON containing style information about nodes.
-		:param positions_json: JSON containing layout positions for the nodes.
 		:param graph_id: ID of the graph.
-		:param layout_name: Name of the layout.
+		:param layout: GSLayout object.
 		:param is_shared: 1 if layout is shared else 0
 		:return: Layout Object
-
-		Sample style_json::
-
-			{
-			    "style": [
-			        {
-			            "selector": "node[name='P4314611']",
-			            "style": {
-			                "border-color": "#888",
-			                "text-halign": "center",
-			                "text-valign": "center",
-			                "border-width": "2px",
-			                "height": "50px",
-			                "width": "50px",
-			                "shape": "ellipse",
-			                "background-blacken": "0.1",
-			                "background-color": "yellow"
-			            }
-			        },
-			        {
-			            "selector": "node[name='P0810711']",
-			            "style": {
-			                "text-halign": "center",
-			                "text-valign": "center",
-			                "text-outline-color": "#888",
-			                "text-outline-width": "2px",
-			                "border-color": "black",
-			                "border-width": "5px",
-			                "height": "150px",
-			                "shape": "ellipse",
-			                "color": "black",
-			                "border-style": "double",
-			                "text-wrap": "wrap",
-			                "background-blacken": "0",
-			                "width": "150px",
-			                "background-color": "red"
-			            }
-			        },
-			        {
-			            "selector": "edge[name='P4314611-P0810711']",
-			            "style": {
-			                "curve-style": "bezier",
-			                "line-style": "dotted",
-			                "width": "12px",
-			                "line-color": "blue",
-			                "source-arrow-color": "yellow",
-			                "target-arrow-shape": "triangle"
-			            }
-			        }
-			    ]
-			}
-
-		Sample positions_json::
-
-			{
-			    "P4314611": {
-			        "y": 87,
-			        "x": 35
-			    },
-			    "P0810711": {
-			        "y": 87.89306358381505,
-			        "x": 208.18593448940268
-			    }
-			}
-
-
 		"""
 		data = {
-			'name': layout_name,
+			'name': layout.get_name(),
 			'graph_id': graph_id,
 			'is_shared': 0 if is_shared is None else is_shared,
-			'owner_email': self.client.username
+			'owner_email': self.client.username,
+			'style_json': layout.get_style_json(),
+			'positions_json': layout.get_positions_json
 		}
-
-		if positions_json is not None:
-			data.update({'positions_json': positions_json})
-		else:
-			data.update({'positions_json': {}})
-		if style_json is not None:
-			data.update({'style_json': style_json})
-		else:
-			data.update({'style_json': {'style': []}})
 
 		layouts_path = LAYOUTS_PATH.format(graph_id)
 		return LayoutResponse(
 			self.client._make_request("POST", layouts_path, data=data).json()
 		)
 
-	def update_graph_layout(self, graph_id, layout_id, layout_name=None, positions_json=None, style_json=None, is_shared=None):
+	def update_graph_layout(self, graph_id, layout_id, layout=None, is_shared=None):
 		"""Update layout with given layout_id for the graph with given graph_id.
 
 		:param layout_id: ID of the layout.
-		:param style_json: JSON containing style information about nodes.
-		:param positions_json: JSON containing layout positions for the nodes.
 		:param graph_id: ID of the graph.
-		:param layout_name: Name of the layout.
+		:param layout: GSLayout object.
 		:param is_shared: 1 if layout is shared else 0
 		:return: Layout Object
-
-		Sample style_json::
-
-			{
-			    "style": [
-			        {
-			            "selector": "node[name='P4314611']",
-			            "style": {
-			                "border-color": "#888",
-			                "text-halign": "center",
-			                "text-valign": "center",
-			                "border-width": "2px",
-			                "height": "50px",
-			                "width": "50px",
-			                "shape": "ellipse",
-			                "background-blacken": "0.1",
-			                "background-color": "yellow"
-			            }
-			        },
-			        {
-			            "selector": "node[name='P0810711']",
-			            "style": {
-			                "text-halign": "center",
-			                "text-valign": "center",
-			                "text-outline-color": "#888",
-			                "text-outline-width": "2px",
-			                "border-color": "black",
-			                "border-width": "5px",
-			                "height": "150px",
-			                "shape": "ellipse",
-			                "color": "black",
-			                "border-style": "double",
-			                "text-wrap": "wrap",
-			                "background-blacken": "0",
-			                "width": "150px",
-			                "background-color": "red"
-			            }
-			        },
-			        {
-			            "selector": "edge[name='P4314611-P0810711']",
-			            "style": {
-			                "curve-style": "bezier",
-			                "line-style": "dotted",
-			                "width": "12px",
-			                "line-color": "blue",
-			                "source-arrow-color": "yellow",
-			                "target-arrow-shape": "triangle"
-			            }
-			        }
-			    ]
-			}
-
-		Sample positions_json::
-
-			{
-			    "P4314611": {
-			        "y": 87,
-			        "x": 35
-			    },
-			    "P0810711": {
-			        "y": 87.89306358381505,
-			        "x": 208.18593448940268
-			    }
-			}
-
 		"""
-		data = {}
-
-		if layout_name is not None:
-			data.update({'name': layout_name})
-		if is_shared is not None:
-			data.update({'is_shared': is_shared})
-		if positions_json is not None:
-			data.update({'positions_json': positions_json})
-		if style_json is not None:
-			data.update({'style_json': style_json})
+		if layout is not None:
+			data = {
+				'name': layout.get_name(),
+				'is_shared': 0 if is_shared is None else is_shared,
+				'positions_json': layout.get_positions_json(),
+				'style_json': layout.get_style_json()
+			}
+		else:
+			data = {
+				'is_shared': 0 if is_shared is None else is_shared
+			}
 
 		layout_by_id_path = LAYOUTS_PATH.format(graph_id) + str(layout_id)
 		return LayoutResponse(
--- graphspace_python/api/obj/layout.py
+++ graphspace_python/api/obj/layout.py
@@ -1,6 +1,8 @@
+import json
 from graphspace_python.api.obj.response_object import ResponseObject
+from graphspace_python.graphs.classes.gslayout import GSLayout
 
-class Layout(ResponseObject):
+class Layout(ResponseObject, GSLayout):
 
     _fields = [
         'id',
@@ -15,4 +17,11 @@ class Layout(ResponseObject):
     ]
 
     def __init__(self, response):
-        super(Layout, self).__init__(response)
+        GSLayout.__init__(self)
+        ResponseObject.__init__(self, response)
+        self._convert_string_to_json()
+
+    def _convert_string_to_json(self):
+        # Convert the positions_json and style_json strings in the response to json dict
+        self.style_json = json.loads(self.style_json)
+        self.positions_json = json.loads(self.positions_json)
